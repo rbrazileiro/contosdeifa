fe72fcac07e902365c119c24ab623652b6166a32
rbrz <rbrazileiro@gmail.com>
2013-11-16 22:56:27 -0300

    add some features

--- js/contosdeifa.js
+++ js/contosdeifa.js
@@ -1,14 +1,29 @@
 $( document ).ready(function() {
- 	$('body').starscroll(
-	    16,
-	    3,
-	    10,
-	    5,
-	    5,
-	    [193,198,150],
-	    false,
-	    true
-	); 
+ // 	$('body').starscroll(
+	//     16,
+	//     3,
+	//     10,
+	//     5,
+	//     5,
+	//     [193,198,150],
+	//     false,
+	//     true
+	// ); 
+
+	// $('.ifa').parallax({
+	// 	calibrateX: false,
+	// 	calibrateY: true,
+	// 	invertX: false,
+	// 	invertY: true,
+	// 	limitX: false,
+	// 	limitY: 10,
+	// 	scalarX: 2,
+	// 	scalarY: 8,
+	// 	frictionX: 0.2,
+	// 	frictionY: 0.8
+	// });
+
+	var s = skrollr.init();
 
 
 
