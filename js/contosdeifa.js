f3904082912d0b8b9a2d3a6769d6bad16a53e447
rbrz <rbrazileiro@gmail.com>
2013-11-26 14:26:32 -0300

    add new pseudo-indexx, particles js and others stuffs.

--- js/contosdeifa.js
+++ js/contosdeifa.js
@@ -1,5 +1,5 @@
 $( document ).ready(function() {
- // 	$('body').starscroll(
+ 	// 	$('body').starscroll(
 	//     16,
 	//     3,
 	//     10,
@@ -23,7 +23,31 @@ $( document ).ready(function() {
 	// 	frictionY: 0.8
 	// });
 
+	// Skrollr Libs
 	var s = skrollr.init();
+
+	// Big Bang using Particles.js
+
+	$(window).scroll(function(){
+		if ($(window).scrollTop() == 0){
+            bigbang.stop();
+        }
+
+        if ($(window).scrollTop() > 50){
+            bigbang.start();
+        }
+        if ($(window).scrollTop() > 250){
+            bigbang.stop();
+        }
+    });
+
+	var bigbang = new particle_emitter({
+    	image: ['imgs/intro/dende.png'],
+    	center: ['50%', '50%'], offset: [0, 0], radius: 0,
+    	size: 36, velocity: 500, decay: 3000, rate: 70
+    });
+
+	
 
 
 
