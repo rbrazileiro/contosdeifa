fb2c1c4c51e5a63960157845720b63b1dd90493e
Ricardo Brazileiro <rbrazileiro@gmail.com>
2013-11-26 16:05:18 -0300

    bug intro fixed

--- js/contosdeifa.js
+++ js/contosdeifa.js
@@ -1,29 +1,5 @@
 $( document ).ready(function() {
- 	// 	$('body').starscroll(
-	//     16,
-	//     3,
-	//     10,
-	//     5,
-	//     5,
-	//     [193,198,150],
-	//     false,
-	//     true
-	// ); 
-
-	// $('.ifa').parallax({
-	// 	calibrateX: false,
-	// 	calibrateY: true,
-	// 	invertX: false,
-	// 	invertY: true,
-	// 	limitX: false,
-	// 	limitY: 10,
-	// 	scalarX: 2,
-	// 	scalarY: 8,
-	// 	frictionX: 0.2,
-	// 	frictionY: 0.8
-	// });
-
-	// Skrollr Libs
+	// Skrollr
 	var s = skrollr.init();
 
 	// Big Bang using Particles.js
@@ -46,6 +22,8 @@ $( document ).ready(function() {
     	center: ['50%', '50%'], offset: [0, 0], radius: 0,
     	size: 36, velocity: 500, decay: 3000, rate: 70
     });
+
+
 
 	
 
