bc538df7d3c0bfad80cdd593a6fb6b17161df0db
Ricardo Brazileiro <rbrazileiro@gmail.com>
2014-02-10 15:48:46 -0300

    node-syslog

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 
 var express = require("express");
+var Syslog = require("node-syslog");
 var app = express();
 
 app.get('/', function (req, res) {
@@ -10,5 +11,10 @@ app.get('/', function (req, res) {
 app.get('/*.(js|css|png|jpg|mp3|woff|ttf)', function(req, res){
   res.sendfile(__dirname + '/assets'+req.url);
 });
+Syslog.init("node-syslog-test", Syslog.LOG_PID | Syslog.LOG_ODELAY, Syslog.LOG_LOCAL0);
+Syslog.log(Syslog.LOG_INFO, "news info log test");
+Syslog.log(Syslog.LOG_ERR, "news log error test");
+Syslog.log(Syslog.LOG_DEBUG, "Last log message as debug: " + new Date());
+Syslog.close();
 
 app.listen(80);
