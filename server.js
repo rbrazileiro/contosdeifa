55149f0c37d16243641262eb61991b098445ee7f
Ricardo Brazileiro <rbrazileiro@gmail.com>
2014-02-12 19:21:00 -0300

    error handler

--- server.js
+++ server.js
@@ -3,6 +3,14 @@ var express = require("express");
 var Syslog = require("node-syslog");
 var app = express();
 
+app.use(app.router); // you need this line so the .get etc. routes are run and if an error within, then the error is parsed to the ned middleware (your error reporter)
+app.use(function(err, req, res, next) {
+    if(!err) return next(); // you also need this line
+    Syslog.init("node-syslog-test", Syslog.LOG_PID | Syslog.LOG_ODELAY, Syslog.LOG_LOCAL0);
+    Syslog.log(Syslog.LOG_ERR, err.stack);
+    Syslog.close();
+});
+
 app.get('/', function (req, res) {
   res.sendfile(__dirname + '/index.html');
 });
@@ -11,10 +19,5 @@ app.get('/', function (req, res) {
 app.get('/*.(js|css|png|jpg|mp3|woff|ttf|gif)', function(req, res){
   res.sendfile(__dirname + '/assets'+req.url);
 });
-Syslog.init("node-syslog-test", Syslog.LOG_PID | Syslog.LOG_ODELAY, Syslog.LOG_LOCAL0);
-Syslog.log(Syslog.LOG_INFO, "news info log test");
-Syslog.log(Syslog.LOG_ERR, "news log error test");
-Syslog.log(Syslog.LOG_DEBUG, "Last log message as debug: " + new Date());
-Syslog.close();
 
 app.listen(80);
